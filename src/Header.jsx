7a5f22eb8d9c4047ac205ace884e82c922415300
Sayaib Sarkar <66420972+sayaib@users.noreply.github.com>
2021-12-04 23:56:16 +0530

    code cleaning

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./Header.css";
 import MenuIcon from "@mui/icons-material/Menu";
 import SearchIcon from "@mui/icons-material/Search";
+import MicIcon from "@mui/icons-material/Mic";
 import VideoCallIcon from "@mui/icons-material/VideoCall";
 import AppsIcon from "@mui/icons-material/Apps";
 import NotificationsIcon from "@mui/icons-material/Notifications";
@@ -14,22 +15,24 @@ function Header() {
         <MenuIcon />
         <img
           className="header_logo"
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Logo_of_YouTube_%282015-2017%29.svg/502px-Logo_of_YouTube_%282015-2017%29.svg.png"          alt=""
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Logo_of_YouTube_%282015-2017%29.svg/502px-Logo_of_YouTube_%282015-2017%29.svg.png"
+          alt=""
         />
       </div>
 
       <div className="header_input">
         <input placeholder="Search" type="text" name="" id="" />
-        <SearchIcon className="header_inputButton"/>
+        <SearchIcon className="header_inputButton" />
+        <MicIcon />
       </div>
 
       <div className="header_icons">
-        <VideoCallIcon className="header_icon"/>
-        <AppsIcon className="header_icon"/>
-        <NotificationsIcon className="header_icon"/>
+        <VideoCallIcon className="header_icon" />
+        <AppsIcon className="header_icon" />
+        <NotificationsIcon className="header_icon" />
         <Avatar
           alt="Remy Sharp"
-          src="https://www.citypng.com/public/uploads/preview/-51609516230nkazrdjky9.png"
+          src="https://www.deccanherald.com/sites/dh/files/styles/article_detail/public/article_images/2014/06/11/412919.gif?itok=ZXolMLj7"
         />
       </div>
     </div>
