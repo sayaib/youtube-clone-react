4017eece0ff5d5d98517aa5f9133594894f2880c
Sayaib Sarkar <66420972+sayaib@users.noreply.github.com>
2021-12-04 22:56:09 +0530

    header  code without css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
+import Header from './Header';
 
 function App() {
-return (
-  <>
-  <h1>Sayaib Sarkar</h1>
-    
-  </>
-);
+  return (
+    <div className="app">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
