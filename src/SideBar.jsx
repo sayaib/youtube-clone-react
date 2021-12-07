a64c6f680a4b1ee2fe02d4d0a68a8bbd7ba7e779
Sayaib Sarkar <66420972+sayaib@users.noreply.github.com>
2021-12-07 18:24:58 +0530

    update

--- src/SideBar.jsx
+++ src/SideBar.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import "./SideBar.css";
 import SideBarCard from "./SideBarCard";
-import HomeIcon from '@mui/icons-material/Home';
-import ExploreIcon from '@mui/icons-material/Explore';
-import SubscriptionsIcon from '@mui/icons-material/Subscriptions';
+import HomeIcon from "@mui/icons-material/Home";
+import ExploreIcon from "@mui/icons-material/Explore";
+import SubscriptionsIcon from "@mui/icons-material/Subscriptions";
 
 function SideBar() {
   return (
     <div className="sidebar">
-      <SideBarCard Icon={HomeIcon} name="Home"/>
-      <SideBarCard Icon={ExploreIcon} name="Explore"/>
-      <SideBarCard Icon={SubscriptionsIcon} name="Subscriptions"/>
+      <SideBarCard Icon={HomeIcon} name="Home" />
+      <SideBarCard Icon={ExploreIcon} name="Explore" />
+      <SideBarCard Icon={SubscriptionsIcon} name="Subscriptions" />
+      <hr />
     </div>
   );
 }
