b41ce19177cc199da2b367227362db1a96ab069e
Sayaib Sarkar <66420972+sayaib@users.noreply.github.com>
2021-12-07 23:14:03 +0530

    update video thumblines

--- src/RecommendVideos.jsx
+++ src/RecommendVideos.jsx
@@ -1,16 +1,24 @@
-import React from 'react';
-import './RecommendVideos.css';
-import VideoCard from './VideoCard';
+import React from "react";
+import "./RecommendVideos.css";
+import VideoCard from "./VideoCard";
 
 function RecommendVideos() {
-    return (
-        <div className="recommendVideos">
-            <h1>Recommended</h1>
-            <div className="recommendVideos_video">
-                <VideoCard/>
-            </div>
-        </div>
-    )
+  return (
+    <div className="recommendVideos">
+      <h1>Recommended</h1>
+      <div className="recommendVideos_video">
+
+        <VideoCard
+        img="https://www.91-cdn.com/hub/wp-content/uploads/2021/03/YouTube-Thumbnail-maker.jpeg" 
+        avatar="https://scontent.fccu20-1.fna.fbcdn.net/v/t1.6435-1/s200x200/151118557_2838457176402744_6982790705069458652_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=7206a8&_nc_ohc=0An3QqzYxeMAX9_PaJO&_nc_ht=scontent.fccu20-1.fna&oh=9dfa5750e47dbadf12357f929b4e1978&oe=61D52C77" 
+        title="How to create youtube videos details with proper explanation."
+        channelName="Sayaib Sarkar"
+        views="2.1M views"
+        postedTime="11 days ago"
+        />
+      </div>
+    </div>
+  );
 }
 
-export default RecommendVideos
+export default RecommendVideos;
--- src/VideoCard.jsx
+++ src/VideoCard.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
-import './VideoCard.css'
+import React from "react";
+import "./VideoCard.css";
+import Avatar from "@mui/material/Avatar";
 
-function VideoCard() {
-    return (
-        <div>
-            
+function VideoCard({ img, title, avatar, channelName, views, postedTime }) {
+  return (
+    <div className="videoCard">
+      <img className="thumbnailImage" src={img} alt="" />
+      <div className="videoDetails">
+        <Avatar src={avatar} />
+        <div className="channelDetails">
+          <h4>{title}</h4>
+          <p>{channelName}</p>
+          <p>
+            {views}.{postedTime}
+          </p>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default VideoCard
+export default VideoCard;
